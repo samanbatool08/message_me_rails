97f59393f29b3b0246c1831b7723d6fb29ed84cf
Saman Batool <samanbatool08@gmail.com>
2022-08-23 20:02:47 -0400

    add auto-scrolling to chat window

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,14 +13,20 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+function scroll_bottom() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  }
+}
+
 // this will enable the dom to load before the dropdown logic 
 $(document).on("turbolinks:load", () => {
   $(".ui.dropdown").dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
-  })
+  });
+  scroll_bottom();
 });
-
 
 
 
